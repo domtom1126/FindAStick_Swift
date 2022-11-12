fde3ef0d6b73f6edde053dc8de41155ef346ce8a
dom thomas <domtom1126@gmail.com>
2022-11-12 17:04:12 -0500

    Initial Commit

---
+++ FindAStick/FindAStickApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindAStickApp.swift
+//  FindAStick
+//
+//  Created by Dominic . on 11/12/22.
+//
+
+import SwiftUI
+
+@main
+struct FindAStickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
